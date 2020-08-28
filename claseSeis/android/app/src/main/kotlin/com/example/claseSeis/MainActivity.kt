df34f7249e9947025baea4568fe0f2768ecfe2db
samil96 <1628112@utp.edu.pe>
2020-08-27 22:14:38 -0500

    clase seis

---
+++ claseSeis/android/app/src/main/kotlin/com/example/claseSeis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.claseSeis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ claseSeis/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'page/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
